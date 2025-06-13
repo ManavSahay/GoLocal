15f7eae7887c8c7a10f9f2e16505c764cda155bb
ManavSahay <manavsahay4885@gmail.com>
2025-06-13 18:17:37 +0530

    Rating repository created and Create Rating DTO created

--- GoLocal/src/main/java/com/pentagon/golocal/repository/CustomerRepository.java
+++ GoLocal/src/main/java/com/pentagon/golocal/repository/CustomerRepository.java
@@ -3,7 +3,7 @@ package com.pentagon.golocal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pentagon.golocal.entity.Customer;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, String> {
-
 }
